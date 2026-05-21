f2375619de38baed23d295afacd12833f8691b42
Daina-Jahaveth <yahavethdayna@gmail.com>
2026-05-21 11:37:55 -0600

    fix render database

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host:     process.env.DB_HOST     || 'localhost',
-  port:     parseInt(process.env.DB_PORT || '5433'),
-  database: process.env.DB_NAME     || 'psicologia_escolar',
-  user:     process.env.DB_USER     || 'postgres',
-  password: process.env.DB_PASSWORD || '1234',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
